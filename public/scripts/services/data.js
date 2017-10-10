c0f01bbd420c29f82672360eefc1d089699fd395
chelseaclay <chelseaclayyy@yahoo.com>
2017-10-10 09:25:38 -0400

    Cleaning up code

--- public/scripts/services/data.js
+++ public/scripts/services/data.js
@@ -6,48 +6,48 @@
 
       // Gets all of the recipes
       this.allRecipes = function(callback) {
-        $http.get('http://localhost:5000/api/recipes')
-          .then(callback)
+        $http.get('api/recipes')
+          .then(callback);
       };
 
       // Gets all of the categories
       this.allCategories = function(callback) {
-        $http.get('http://localhost:5000/api/categories')
-          .then(callback)
+        $http.get('api/categories')
+          .then(callback);
       };
 
       // Gets all of the food items
       this.allFood = function(callback) {
-        $http.get('http://localhost:5000/api/fooditems')
-          .then(callback)
+        $http.get('api/fooditems')
+          .then(callback);
       };
 
       // Gets all of the recipes for the specified category
       this.allRecipesForCategory = function(category, callback) {
-        $http.get(`http://localhost:5000/api/recipes?category=${category.name}`)
-          .then(callback)
+        $http.get(`api/recipes?category=${category.name}`)
+          .then(callback);
       };
 
       // Gets the recipe for the specified ID
       this.recipeForID = function(id, callback) {
-        $http.get(`http://localhost:5000/api/recipes/${id}`)
-          .then(callback)
+        $http.get(`api/recipes/${id}`)
+          .then(callback);
       };
 
       // Updates the recipe for the specified ID
       this.updateRecipeForID = function(recipe) {
-        $http.put(`http://localhost:5000/api/recipes/${recipe._id}`, recipe)
+        $http.put(`api/recipes/${recipe._id}`, recipe);
       };
 
       // Adds a recipe
       this.addRecipe = function(recipe) {
-        $http.post('http://localhost:5000/api/recipes/', recipe)
+        $http.post('api/recipes/', recipe);
       };
 
       // Deletes the recipe for the specified ID
       this.deleteRecipeForID = function(recipe, callback) {
-        $http.delete(`http://localhost:5000/api/recipes/${recipe._id}`)
-          .then(callback)
+        $http.delete(`api/recipes/${recipe._id}`)
+          .then(callback);
       };
 
     });
